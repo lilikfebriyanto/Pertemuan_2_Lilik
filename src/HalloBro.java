225857dbbc8c17cd026b0c00de2f53940f28db6d
agengrikh <srujacxtup@gmail.com>
2016-03-23 13:22:55 +0700

    Commit3

---
+++ src/HalloBro.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Agengrikh on 23/03/2016.
+ */
+public class HalloBro {
+
+    public static void main(String[] args) {
+        // Cetak "Hello, World" ke Terminal Windows.
+        //!ganti "Hello World" dengan Nama Anda kemudian Run
+        System.out.println("Hello, World");
+    }
+
+}
