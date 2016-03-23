c8b5d888358348db1af7c0028b3164fb520fde60
Lilik Febriyanto <lilikfebriyanto04@gmail.com>
2016-03-23 17:46:20 +0700

    commit 2

--- src/HalloBro.java
+++ src/HalloBro.java
@@ -8,7 +8,7 @@ public class HalloBro {
         //1. ganti "Hello World" dengan Nama Anda kemudian Run
         //2. Push ke Github
 
-        System.out.println("Hello, World");
+        System.out.println("Lilik Febriyanto");
     }
 
 }
